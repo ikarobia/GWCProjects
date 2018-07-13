0d066bff7a8a47d2a75617757ee82c6dfc9ce13f
ikarobia <ikarobia01@gmail.com>
2018-07-13 11:50:03 -0400

    final commit

--- haiku.py
+++ haiku.py
@@ -1,3 +1,8 @@
 from random import *
 line1 = ["An ocean voyage", "On a silent pond", "After the rain"]
 line2 = ["It's life in the sea and murmurs", "of the magic inside you", "Sunlight dancing on waters"]
+line3 = ["Blue water shimmers", "What am I", "I lay still"]
+aRandomIndex1 = randint(0, len(line1)-1)
+aRandomIndex2 = randint(0, len(line2)-1)
+aRandomIndex3 = randint(0, len(line3)-1)
+print(line1[aRandomIndex1], line2[aRandomIndex2], line3[aRandomIndex3])
