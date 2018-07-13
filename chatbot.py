99b747776a31032354c17528ba5a0fa8847673bc
ikarobia <ikarobia01@gmail.com>
2018-07-13 11:24:35 -0400

    initial commit

---
+++ chatbot.py
@@ -0,0 +1,24 @@
+# --- Define your functions below! ---
+
+
+def intro():
+    print("Hello!")
+
+def process.Input(answer):
+    if answer == "hi":
+        saygreeting()
+    else:
+        saydefault()
+def saydefault():
+    print("That's cool!")
+def sa
+def main():
+  while aTrue:
+      processinput(answer)
+    answer = input("(What will you say?) ")
+    print("That's cool!")
+
+
+# DON'T TOUCH! Setup code that runs your main() function.
+if __name__ == "__main__":
+  main()
