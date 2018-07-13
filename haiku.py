405b2fd676a7451a20fcedac66fbd9ca83f520fb
ikarobia <ikarobia01@gmail.com>
2018-07-13 11:43:56 -0400

    I added another line

--- haiku.py
+++ haiku.py
@@ -1,1 +1,3 @@
-line1["An ocean voyage", "On a silent pond", "After the rain"]
+from random import *
+line1 = ["An ocean voyage", "On a silent pond", "After the rain"]
+line2 = ["It's life in the sea and murmurs", "of the magic inside you", "Sunlight dancing on waters"]
